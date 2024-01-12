9edf1aa98d1238c2dbb9d541ec792766eec4c960
Andy Camann <acamann@gmail.com>
2024-01-12 07:23:13 -0600

    clean up

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,18 +1,4 @@
-// import { AuthError, AuthResponse, AuthTokenResponse, User } from "@supabase/supabase-js";
-// import { createContext, useContext, useEffect, useState } from "react";
-// import { useDB } from "./DBContext";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
-
-// type AuthContextType = {
-//   user: User | null,
-//   login: (email: string, password: string) => Promise<AuthTokenResponse>,
-//   register: (email: string, password: string) => Promise<AuthResponse>;
-//   logout: () => Promise<{ error: AuthError | null }>;
-// }
-
-// const AuthContext = createContext<AuthContextType>({} as AuthContextType);
-
-// export const useAuth = () => useContext(AuthContext);
 
 if (!import.meta.env.VITE_AUTH_0_DOMAIN) {
   throw new Error("Missing VITE_AUTH_0_DOMAIN");
@@ -23,7 +9,6 @@ if (!import.meta.env.VITE_AUTH_0_CLIENT_ID) {
   throw new Error("Missing VITE_AUTH_0_CLIENT_ID");
 }
 const clientId = import.meta.env.VITE_AUTH_0_CLIENT_ID;
-
 
 export const useAuth = useAuth0;
 
