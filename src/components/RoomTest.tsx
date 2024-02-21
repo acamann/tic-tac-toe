721a69a824b280df6ec428f3286108973b0bbe18
Andy Camann <acamann@gmail.com>
2024-02-20 20:33:47 -0600

    leave and join room

--- src/components/RoomTest.tsx
+++ src/components/RoomTest.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import "./GameSetup.css";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { RoomEntity } from '../types/models';
@@ -8,26 +8,7 @@ const RoomTest = () => {
   const [rooms, setRooms] = useState<RoomEntity[]>([]);
 
   const { user } = useUser();
-
-  const createRoom = async () => {
-    const resp = await fetch(`/api/rooms`, {
-      method: "POST"
-    });
-    if (!resp.ok) {
-      console.error(resp);
-    }
-  };
-
-  const getRooms = async () => {
-    const resp = await fetch(`/api/rooms`);
-    if (!resp.ok) {
-      console.error(resp);
-      return;
-    }
-
-    const { rooms } = await resp.json() as { rooms: RoomEntity[] };
-    setRooms(rooms);
-  }
+  const userName = useMemo(() => user?.nickname ?? user?.name, [user]);
 
   useEffect(() => {
     getRooms();
@@ -52,6 +33,51 @@ const RoomTest = () => {
     }
   }, [realtimeClient.channels]);
 
+  if (!userName) { 
+    return null;
+  }
+
+  const createRoom = async () => {
+    const resp = await fetch(`/api/rooms`, {
+      method: "POST"
+    });
+    if (!resp.ok) {
+      console.error(resp);
+    }
+    else { await getRooms() } // poor mans realtime
+  };
+
+  const getRooms = async () => {
+    const resp = await fetch(`/api/rooms`);
+    if (!resp.ok) {
+      console.error(resp);
+      return;
+    }
+
+    const { rooms } = await resp.json() as { rooms: RoomEntity[] };
+    setRooms(rooms);
+  }
+
+  const joinRoom = async (id: string) => {
+    const resp = await fetch(`/api/rooms/${id}`, {
+      method: "PUT"
+    });
+    if (!resp.ok) {
+      console.error(resp);
+    }
+    else { await getRooms() } // poor mans realtime
+  };
+
+  const leaveRoom = async (id: string) => {
+    const resp = await fetch(`/api/rooms/${id}`, {
+      method: "DELETE"
+    });
+    if (!resp.ok) {
+      console.error(resp);
+    }
+    else { await getRooms() } // poor mans realtime
+  };
+
   return (
     <div className="setup">
       <div className="user">
@@ -65,7 +91,19 @@ const RoomTest = () => {
       <button onClick={createRoom}>Create Room</button>
       <ul>
         {rooms.map(room => (
-          <li key={room.id}>{room.host} ({room.players.length})</li>
+          <li key={room.id}>
+            {room.host} ({room.players.length})
+            {room.players.length < 2 && !room.players.includes(userName) && (
+              <button onClick={() => joinRoom(room.id)}>
+                Join
+              </button>
+            )}
+            {room.players.includes(userName) && (
+              <button onClick={() => leaveRoom(room.id)}>
+                Leave
+              </button>
+            )}
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/pages/api/rooms/[id]/index.ts
@@ -0,0 +1,146 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createClient } from '@supabase/supabase-js';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { RoomEntity } from './../../../../types/models';
+
+const ROOM_MAX = 2;
+
+if (!process.env.SUPABASE_URL) {
+  throw new Error("Missing required environment variable SUPABASE_URL");
+}
+if (!process.env.SUPABASE_KEY) {
+  throw new Error("Missing required environment variable SUPABASE_KEY");
+}
+
+// Supabase DB - for persisted game state
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default withApiAuthRequired(async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  try {
+    const { id } = request.query;
+    const session = await getSession(request, response);
+
+    if (!session) {
+      return response.status(401);
+    }
+
+    if (request.method === "GET") {
+      const { data, error } = await supabase
+        .from('Rooms')
+        .select()
+        .eq("id", id)
+        .returns<RoomEntity[]>();
+
+      if (!data || data.length === 0) {
+        return response.status(404).json({ message: error?.message ?? "Room not found" });
+      }
+
+      const room = data[0];
+
+      return response.status(200).json(room);
+    } else if (request.method === "PUT") {
+      const player: string = session.user.nickname ?? session.user.name;
+
+      // already in another room?
+
+      // const { data: currentRoomData, error: currentRoomError } = await supabase
+      //   .from('Rooms')
+      //   .select()
+      //   .filter('players', 'cs', `{${player}}`)
+      //   .returns<RoomEntity[]>();
+
+      // if (currentRoomError) {
+      //   return response.status(500).json(currentRoomError)
+      // }
+
+      // if (!currentRoomData || currentRoomData.length > 0) {
+      //   return response.status(409).json({ message: "Already in a room", id: currentRoomData[0].id })
+      // }
+
+      const { data, error: findRoomError } = await supabase
+        .from('Rooms')
+        .select()
+        .eq("id", id)
+        .returns<RoomEntity[]>();
+
+      if (!data || data.length === 0) {
+        return response.status(404).json({ message: findRoomError?.message ?? "Room not found" });
+      }
+
+      const room = data[0];
+
+      if (room.players.includes(player)) {
+        // already in room, no-op for idempotency
+        return response.status(204).end();
+      }
+
+      if (room.players.length >= ROOM_MAX) {
+        return response.status(409).json({ message: "Room is full" });
+      }
+
+      // add self to room
+      const newPlayers = [...room.players, player];
+
+      const { error } = await supabase.from('Rooms')
+        .update({ "players": newPlayers })
+        .eq("id", id);
+
+      if (error) {
+        return response.status(409).json({ message: error.message });
+      }
+
+      return response.status(204).end();
+    } else if (request.method === "DELETE") {
+      const player: string = session.user.nickname ?? session.user.name;
+
+      const { data, error: findRoomError } = await supabase
+        .from('Rooms')
+        .select()
+        .eq("id", id)
+        .returns<RoomEntity[]>();
+
+      if (!data || data.length === 0) {
+        return response.status(404).json({ message: findRoomError?.message ?? "Room not found" });
+      }
+
+      const room = data[0];
+
+      if (!room.players.includes(player)) {
+        // not in room, no-op for idempotency
+        return response.status(204).end();
+      }
+
+      if (room.host === player) {
+        // if host leaves, room is gone
+
+        const { error: deleteError } = await supabase.from('Rooms')
+          .delete()
+          .eq("id", id);
+
+        if (deleteError) {
+          return response.status(409).json({ message: deleteError.message });
+        }
+
+        return response.status(204).end();
+      }
+
+      const newPlayers = room.players.filter(p => p !== player)
+      const { error } = await supabase.from('Rooms')
+        .update({ players: newPlayers })
+        .eq("id", id);
+
+      if (error) {
+        return response.status(409).json({ message: error.message });
+      }
+    } else {
+      return response.status(405);
+    }
+  } catch (e) {
+    return response.status(500).json({ message: "An unknown error occurred" });
+  }
+});
